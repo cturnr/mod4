7e9fc77024fd3cd6202ffb10ab17b647417d307c
Chris Turner <mr.chris.turner@gmail.com>
2016-11-02 21:04:08 -0700

    updated the homework to display the scripts on the webpage as well as the console.

--- SpeakHello.js
+++ SpeakHello.js
@@ -3,6 +3,8 @@
 	var speakWord = "Hello";
 	helloSpeaker.speak = function (name) {
   	console.log(speakWord + " " + name);
+  	document.write(speakWord + " " + name + "<br>");
+  	// document.write("\n");
 	}
 	window.helloSpeaker = helloSpeaker;
 })(window);
